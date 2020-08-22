caa08107311c96d9095773e7c081d20bd573e80d
AUKAMTO <42402703+AUKAMTO@users.noreply.github.com>
2020-08-22 18:47:06 +0800

    A
    A

--- weatherdoc/html/weather/v2/cwget23.py
+++ weatherdoc/html/weather/v2/cwget23.py
@@ -25,7 +25,6 @@ def checkApiJosnNumNull(json_data,x,JsonVal,val):
         val = 0
         return val
 
-
 def checkApiJosnTextNull(json_data,x,JsonVal,val):
     try:
         val = json_data[str(x)]['data'][JsonVal]['value']
@@ -33,50 +32,7 @@ def checkApiJosnTextNull(json_data,x,JsonVal,val):
     except:
         val = "null"
         return val
-        
-def inputdata(table,json_data,x,hour,sec,noSchoolNum,schoolNum):
     
-        noSchoolNum = 0
-        daynum = 0
-        inputtime = 0
-        airTemperature = 0
-        maximumAirTemperature = 0
-        minimumAirTemperature = 0
-        relativeHumidity = 0
-        windSpeed = 0
-        windDirection = "null"
-        past60MinutesRainfall = 0
-        uvIndex = 0
-        maximumUV = 0
-        solarRadiation = 0
-        seaLevelPressure = 0
-        daynum =  str(yesterday)
-        inputtime =  str(hour)  + str(sec)
-        inputtime = int(inputtime)
-        inputtime = str(inputtime)
-        
-        if noSchoolNum != schoolNum:
-            airTemperature = str(checkApiJosnNumNull(json_data,x,'Air Temperature',airTemperature))
-            maximumAirTemperature = str(checkApiJosnNumNull(json_data,x,'Maximum Air Temperature',maximumAirTemperature))
-            minimumAirTemperature = str(checkApiJosnNumNull(json_data,x,'Minimum Air Temperature',minimumAirTemperature))
-            relativeHumidity = str(checkApiJosnNumNull(json_data,x,'Relative Humidity',relativeHumidity))
-            windSpeed = str(checkApiJosnNumNull(json_data,x,'Wind Speed',windSpeed))
-            windDirection = str(checkApiJosnTextNull(json_data,x,'Wind Direction',windDirection))
-            past60MinutesRainfall = str(checkApiJosnNumNull(json_data,x,'Past 60-Minutes Rainfall',past60MinutesRainfall))
-            uvIndex = str(checkApiJosnNumNull(json_data,x,'UV Index',uvIndex))
-            maximumUV = str(checkApiJosnNumNull(json_data,x,'Maximum UV',maximumUV))
-            solarRadiation = str(checkApiJosnNumNull(json_data,x,'Solar Radiation',solarRadiation))
-            seaLevelPressure = str(checkApiJosnNumNull(json_data,x,'Sea-Level Pressure',seaLevelPressure))
-        
-        item = {
-                'daynum': daynum,'inputtime' : inputtime ,'airTemperature':  (airTemperature),
-                'maximumAirTemperature':(maximumAirTemperature),'minimumAirTemperature':(minimumAirTemperature),
-                'relativeHumidity':(relativeHumidity),'windSpeed':(windSpeed),'windDirection':windDirection,
-                'past60MinutesRainfall':(past60MinutesRainfall),'uvIndex':(uvIndex),'maximumUV':(maximumUV),
-                'solarRadiation':(solarRadiation),'seaLevelPressure':(seaLevelPressure)}
-        table.put_item(Item=item)
-    
-
 def lambda_handler(event, context):
     client = boto3.resource('dynamodb')
     table = client.Table(os.environ["weatherDb"])
@@ -93,9 +49,46 @@ def lambda_handler(event, context):
             noSchoolNum = 0
             print(qUrl)
             for x in range(schoolNum):
-                if schoolName == json_data[str(x)]['station_name']:
-                    inputdata(table,json_data,x,hour,sec,noSchoolNum,schoolNum)
+                if schoolName == json_data[str(x)]['station_name']  :
+                    noSchoolNum = 0
+                    daynum = 0
+                    inputtime = 0
+                    airTemperature = 0
+                    maximumAirTemperature = 0
+                    minimumAirTemperature = 0
+                    relativeHumidity = 0
+                    windSpeed = 0
+                    windDirection = "null"
+                    past60MinutesRainfall = 0
+                    uvIndex = 0
+                    maximumUV = 0
+                    solarRadiation = 0
+                    seaLevelPressure = 0
+                    daynum =  str(yesterday)
+                    inputtime =  str(hour)  + str(sec)
+                    inputtime = int(inputtime)
+                    inputtime = str(inputtime)
+                    
+                    if noSchoolNum != schoolNum:
+                        airTemperature = str(checkApiJosnNumNull(json_data,x,'Air Temperature',airTemperature))
+                        maximumAirTemperature = str(checkApiJosnNumNull(json_data,x,'Maximum Air Temperature',maximumAirTemperature))
+                        minimumAirTemperature = str(checkApiJosnNumNull(json_data,x,'Minimum Air Temperature',minimumAirTemperature))
+                        relativeHumidity = str(checkApiJosnNumNull(json_data,x,'Relative Humidity',relativeHumidity))
+                        windSpeed = str(checkApiJosnNumNull(json_data,x,'Wind Speed',windSpeed))
+                        windDirection = str(checkApiJosnTextNull(json_data,x,'Wind Direction',windDirection))
+                        past60MinutesRainfall = str(checkApiJosnNumNull(json_data,x,'Past 60-Minutes Rainfall',past60MinutesRainfall))
+                        uvIndex = str(checkApiJosnNumNull(json_data,x,'UV Index',uvIndex))
+                        maximumUV = str(checkApiJosnNumNull(json_data,x,'Maximum UV',maximumUV))
+                        solarRadiation = str(checkApiJosnNumNull(json_data,x,'Solar Radiation',solarRadiation))
+                        seaLevelPressure = str(checkApiJosnNumNull(json_data,x,'Sea-Level Pressure',seaLevelPressure))
+                    
+                    item = {
+                            'daynum': daynum,'inputtime' : inputtime ,'airTemperature':  (airTemperature),
+                            'maximumAirTemperature':(maximumAirTemperature),'minimumAirTemperature':(minimumAirTemperature),
+                            'relativeHumidity':(relativeHumidity),'windSpeed':(windSpeed),'windDirection':windDirection,
+                            'past60MinutesRainfall':(past60MinutesRainfall),'uvIndex':(uvIndex),'maximumUV':(maximumUV),
+                            'solarRadiation':(solarRadiation),'seaLevelPressure':(seaLevelPressure)}
+                    table.put_item(Item=item)
                 else:
                     noSchoolNum = noSchoolNum + 1
-                    inputdata(table,json_data,x,hour,sec,noSchoolNum,schoolNum)
 
